0ab77e2547d63616d73cb5d7dc89b16e64b55c31
Ahmed <a.fayed2017@gmail.com>
2025-03-07 02:02:29 +0200

    first commit

---
+++ Program2.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfNumbers = new List<int>() { 5, 6, 7, 8, 9 };
+            Console.Write("Enter the number to search for: ");
+            int searchNumber = Convert.ToInt32(Console.ReadLine());
+
+            bool found = false;
+
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+                if (listOfNumbers[i] == searchNumber)
+                {
+                    Console.WriteLine($"Number {searchNumber} found at index {i}.");
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Number not found in the list.");
+            }
+
+        }
+    }
+}
